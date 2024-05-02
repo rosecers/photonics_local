d76c4f1e13a20369f3fec3fbbb14a1cd7fb30c0d
Rose K. Cersonsky <47536110+rosecers@users.noreply.github.com>
2024-05-02 11:38:58 -0500

    Three more pdos, adding figures and kpoints to project file, min/max change

--- project.pdos.py
+++ project.pdos.py
@@ -3,8 +3,9 @@ import numpy as np
 import signac
 
 project = signac.get_project("/Users/rca/periodic_structures/")
-MIN_PHI = project.document['min_phi']
-MAX_PHI = project.document['max_phi']
+MIN_PHI = project.document["min_phi"]
+MAX_PHI = project.document["max_phi"]
+
 
 class MyProject(FlowProject):
     pass
@@ -27,16 +28,42 @@ def all_pdos_done(job):
                 return False
     return True
 
+
 @MyProject.post(lambda job: "kpoints" in job.document)
 @MyProject.operation
 def assign_kpoints(job):
     import seekpath
+
+    kp_dict = {
+        "GAMMA": [0, 0, 0],
+        "X": [0.0, 0.5, 0.0],
+        "M": [0.5, 0.5, 0.0],
+        "R": [0.5, 0.5, 0.5],
+        "L": [0.5, 0.5, 0.5],
+        "W": [0.5, 0.25, 0.75],
+        "U": [0.625, 0.25, 0.625],
+        "K": [0.375, 0.375, 0.75],
+        "H": [0.5, -0.5, 0.5],
+        "P": [0.25, 0.25, 0.25],
+        "N": [0.0, 0.0, 0.5],
+    }
     kpath = seekpath.getpaths.get_path(
-        (job.sp.lattice_vectors, job.sp.basis, [1 for b in job.sp.basis])
+        (job.sp.lattice_vectors, job.sp.basis, [1 for _ in job.sp.basis])
     )
+
     kpoints = kpath["point_coords"]
-    job.document["kpoints"] = kpoints
+    kplabels = []
+    for p in kpath["path"]:
+        for j in p:
+            if len(kplabels) == 0 or kplabels[-1] != j:
+                kplabels.append(j)
+
+    job.document["kp_labels"] = kplabels
+    job.document["kpoints"] = [
+        kpoints[k] if k in kpoints else kp_dict[k] for k in job.document["kp_labels"]
+    ]
     job.document["kpoints_from"] = "seekpath, 2024"
+
 
 @MyProject.pre(lambda job: "kpoints" in job.document)
 @MyProject.post.isfile("pdos.npz")
@@ -52,21 +79,112 @@ def compute_pdos(job):
 @MyProject.pre(all_pdos_done)
 @MyProject.operation
 def purge_bad_pdos(job):
-    if not job.isfile('pdos.npz'):
-        return 
-    
+    if not job.isfile("pdos.npz"):
+        return
+
     pdos_dict_raw = dict(np.load(job.fn("pdos.npz"), allow_pickle=True))
+    print(list(pdos_dict_raw.keys()))
     superjob_project = signac.get_project(path=job.fn(""))
     for subjob in list(superjob_project.find_jobs({"doc.fill_fraction.$gt": MAX_PHI})):
         key = str((subjob.sp.radius, subjob.sp.dielectric))
+        print(key, key in pdos_dict_raw)
         if key in pdos_dict_raw:
             print(key, pdos_dict_raw.pop(key))
     np.savez(job.fn("pdos.npz"), **pdos_dict_raw)
     for subjob in list(superjob_project.find_jobs({"doc.fill_fraction.$lt": MIN_PHI})):
         key = str((subjob.sp.radius, subjob.sp.dielectric))
+        print(key, key in pdos_dict_raw)
         if key in pdos_dict_raw:
             print(key, pdos_dict_raw.pop(key))
     np.savez(job.fn("pdos.npz"), **pdos_dict_raw)
+
+
+@MyProject.label
+def images_stored(job):
+    image_files = ["gdos_logeps.png", "gdos_eps.png", "dos_logeps.png", "dos_eps.png"]
+    return all([job.isfile(img) for img in image_files])
+
+
+@MyProject.pre(all_pdos_done)
+@MyProject.post(images_stored)
+@MyProject.operation
+def make_images(job):
+    from matplotlib import pyplot as plt
+
+    epsilon = 16
+    pdos_dict_raw = dict(np.load(job.fn("pdos.npz"), allow_pickle=True))
+    w_bins_Tr = None
+    DOS_Tr = None
+    gDOS = None
+    effective_eps = []
+
+    for i, r, f in zip(
+        range(len(job.document.radii)), job.document.radii, job.document.fill_fraction
+    ):
+        key = str((r, epsilon))
+        if key in pdos_dict_raw:
+            w, D, gD = pdos_dict_raw[key]
+            if w_bins_Tr is None:
+                w_bins_Tr = -np.ones((len(job.document.radii), len(w)))
+                DOS_Tr = -np.ones((len(job.document.radii), len(D)))
+                gDOS = -np.ones((len(job.document.radii), len(gD)))
+            w_bins_Tr[i], DOS_Tr[i], gDOS[i] = w, D, gD
+            effective_eps.append(f * epsilon + (1 - f))
+
+    log_eff_eps = np.log10(effective_eps)
+    log_eps_min = np.log10(0.05 * 16 + 0.95)
+    log_eps_max = np.log10(0.95 * 16 + 0.05)
+
+    effective_eps = np.array(effective_eps)
+    eps_min = 0.05 * 16 + 0.95
+    eps_max = 0.95 * 16 + 0.05
+
+    gDOS_copy = gDOS.copy()
+    gDOS_copy[gDOS_copy == -1] = np.nan
+
+    DOS_copy = DOS_Tr.copy()
+    DOS_copy[DOS_copy == -1] = np.nan
+
+    for name, e_list, extrema, dos_list in zip(
+        ["gdos_logeps.png", "gdos_eps.png", "dos_logeps.png", "dos_eps.png"],
+        [log_eff_eps, effective_eps, log_eff_eps, effective_eps],
+        [
+            [log_eps_min, log_eps_max],
+            [eps_min, eps_max],
+            [log_eps_min, log_eps_max],
+            [eps_min, eps_max],
+        ],
+        [gDOS_copy, gDOS_copy, DOS_copy, DOS_copy],
+    ):
+        for i, f, w, g in zip(range(len(e_list)), e_list, w_bins_Tr, dos_list):
+            if i == 0:
+                e_extent = sorted(
+                    [0.5 * (f + e_list[i + 1]), f + 0.5 * (f - e_list[i + 1])]
+                )
+            elif i == len(log_eff_eps) - 1:
+                e_extent = sorted(
+                    [0.5 * (f + e_list[i - 1]), f + 0.5 * (f - e_list[i - 1])]
+                )
+            else:
+                e_extent = sorted(
+                    [0.5 * (f + e_list[i - 1]), 0.5 * (f + e_list[i + 1])]
+                )
+
+            plt.imshow(
+                g.reshape(1, -1) / np.nanmax(dos_list),
+                extent=[
+                    w.min() / w_bins_Tr.max(),
+                    w.max() / w_bins_Tr.max(),
+                    *e_extent,
+                ],
+                aspect="auto",
+                vmin=0,
+                vmax=1,
+            )
+
+        plt.gca().set_xlim([0, 1])
+        plt.gca().set_ylim(extrema)
+        plt.savefig(job.fn(name))
 
 
 if __name__ == "__main__":
