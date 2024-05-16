9c7472b613d6fcb12a763bf3d049a234a25c1148
Rose K. Cersonsky <47536110+rosecers@users.noreply.github.com>
2024-05-16 17:45:43 -0500

    formatting pdos.py and removing broken job

--- make_pdos.py
+++ make_pdos.py
@@ -359,7 +359,7 @@ def make_pdos(superjob):
         radii = np.array([job.sp.radius for job in subjobs])
         ff = np.array([job.document.fill_fraction for job in subjobs])
 
-        if e==16 and len(superjob.document.fill_fraction) != len(ff):
+        if e == 16 and len(superjob.document.fill_fraction) != len(ff):
             # inp = input(
             #     "Overwrite ff & radius? {}->{}".format(
             #         len(superjob.document.fill_fraction), len(ff)
