5fb15b26373a877fcd48490e64a777037e9a6f9e
Rose K. Cersonsky <47536110+rosecers@users.noreply.github.com>
2024-06-24 13:47:13 -0500

    Adding pdos for 51f7e35218c17d3fc0057dfcde56de54

---
+++ workspace/51dce1bed744ebea4ee205f17d934d91/fix.sh
@@ -0,0 +1,18 @@
+echo '51dce1bed744ebea4ee205f17d934d91: 2 jobs total'
+echo 3ad57f9f61d0529176fa1775ccbece73
+git add workspace/51dce1bed744ebea4ee205f17d934d91/workspace/3ad57f9f61d0529176fa1775ccbece73/signac_statepoint.json
+python recalculate_band_structure.py -j 51dce1bed744ebea4ee205f17d934d91 -s 3ad57f9f61d0529176fa1775ccbece73 --unique&
+echo ""
+while [[ -z $(grep "done" workspace/51dce1bed744ebea4ee205f17d934d91/workspace/3ad57f9f61d0529176fa1775ccbece73/output.txt) ]]; do sleep 10; echo -e "[1A[0K$(grep "freqs" workspace/51dce1bed744ebea4ee205f17d934d91/workspace/3ad57f9f61d0529176fa1775ccbece73/output.txt | tail -n 1)"; done
+python fix_subjob_doc.py 3ad57f9f61d0529176fa1775ccbece73 51dce1bed744ebea4ee205f17d934d91
+git add workspace/51dce1bed744ebea4ee205f17d934d91/workspace/3ad57f9f61d0529176fa1775ccbece73/signac_job_document.json
+git add workspace/51dce1bed744ebea4ee205f17d934d91/workspace/3ad57f9f61d0529176fa1775ccbece73/output.txt
+echo 3dda5a4f6eae439a95e95271bb382630
+git add workspace/51dce1bed744ebea4ee205f17d934d91/workspace/3dda5a4f6eae439a95e95271bb382630/signac_statepoint.json
+python recalculate_band_structure.py -j 51dce1bed744ebea4ee205f17d934d91 -s 3dda5a4f6eae439a95e95271bb382630 --unique&
+echo ""
+while [[ -z $(grep "done" workspace/51dce1bed744ebea4ee205f17d934d91/workspace/3dda5a4f6eae439a95e95271bb382630/output.txt) ]]; do sleep 10; echo -e "[1A[0K$(grep "freqs" workspace/51dce1bed744ebea4ee205f17d934d91/workspace/3dda5a4f6eae439a95e95271bb382630/output.txt | tail -n 1)"; done
+python fix_subjob_doc.py 3dda5a4f6eae439a95e95271bb382630 51dce1bed744ebea4ee205f17d934d91
+git add workspace/51dce1bed744ebea4ee205f17d934d91/workspace/3dda5a4f6eae439a95e95271bb382630/signac_job_document.json
+git add workspace/51dce1bed744ebea4ee205f17d934d91/workspace/3dda5a4f6eae439a95e95271bb382630/output.txt
+rm /Users/rca/periodic_structures/workspace/51dce1bed744ebea4ee205f17d934d91/fix.sh
