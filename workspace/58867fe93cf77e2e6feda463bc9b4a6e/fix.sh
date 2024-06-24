43a358b888d99fc6cac120ba5884c7fd6aaf9b70
Rose K. Cersonsky <47536110+rosecers@users.noreply.github.com>
2024-06-24 15:23:28 -0500

    Adding pdos for 59a565f387a3807ae7f904bc9abd14b0

---
+++ workspace/58867fe93cf77e2e6feda463bc9b4a6e/fix.sh
@@ -0,0 +1,10 @@
+echo '58867fe93cf77e2e6feda463bc9b4a6e: 1 jobs total'
+echo 3dda5a4f6eae439a95e95271bb382630
+git add workspace/58867fe93cf77e2e6feda463bc9b4a6e/workspace/3dda5a4f6eae439a95e95271bb382630/signac_statepoint.json
+python recalculate_band_structure.py -j 58867fe93cf77e2e6feda463bc9b4a6e -s 3dda5a4f6eae439a95e95271bb382630 --unique&
+echo ""
+while [[ -z $(grep "done" workspace/58867fe93cf77e2e6feda463bc9b4a6e/workspace/3dda5a4f6eae439a95e95271bb382630/output.txt) ]]; do sleep 10; echo -e "[1A[0K$(grep "freqs" workspace/58867fe93cf77e2e6feda463bc9b4a6e/workspace/3dda5a4f6eae439a95e95271bb382630/output.txt | tail -n 1)"; done
+python fix_subjob_doc.py 3dda5a4f6eae439a95e95271bb382630 58867fe93cf77e2e6feda463bc9b4a6e
+git add workspace/58867fe93cf77e2e6feda463bc9b4a6e/workspace/3dda5a4f6eae439a95e95271bb382630/signac_job_document.json
+git add workspace/58867fe93cf77e2e6feda463bc9b4a6e/workspace/3dda5a4f6eae439a95e95271bb382630/output.txt
+rm /Users/rca/periodic_structures/workspace/58867fe93cf77e2e6feda463bc9b4a6e/fix.sh
