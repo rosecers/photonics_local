4efbf1a4814668bcdf5baf4651d123b77d8045a4
Rose K. Cersonsky <47536110+rosecers@users.noreply.github.com>
2024-06-26 17:14:04 -0500

    Adding jobs for cc94496b6931bc6d0124c7abaf513f76

--- workspace/42e6efd660ad561189afc8603031e9f1/fix.sh
+++ workspace/42e6efd660ad561189afc8603031e9f1/fix.sh
@@ -1,4 +1,4 @@
-echo '42e6efd660ad561189afc8603031e9f1: 6 jobs total'
+echo '42e6efd660ad561189afc8603031e9f1: 7 jobs total'
 echo 1c28bef7db9c4daa8ac99a359d158965
 git add workspace/42e6efd660ad561189afc8603031e9f1/workspace/1c28bef7db9c4daa8ac99a359d158965/signac_statepoint.json
 python recalculate_band_structure.py -j 42e6efd660ad561189afc8603031e9f1 -s 1c28bef7db9c4daa8ac99a359d158965 --unique&
@@ -39,6 +39,14 @@ while [[ -z $(grep "done" workspace/42e6efd660ad561189afc8603031e9f1/workspace/7
 python fix_subjob_doc.py 729f59b3422cdc3f37df6e628f76f956 42e6efd660ad561189afc8603031e9f1
 git add workspace/42e6efd660ad561189afc8603031e9f1/workspace/729f59b3422cdc3f37df6e628f76f956/signac_job_document.json
 git add workspace/42e6efd660ad561189afc8603031e9f1/workspace/729f59b3422cdc3f37df6e628f76f956/output.txt
+echo b082491da2aefebe79e36e2067d59012
+git add workspace/42e6efd660ad561189afc8603031e9f1/workspace/b082491da2aefebe79e36e2067d59012/signac_statepoint.json
+python recalculate_band_structure.py -j 42e6efd660ad561189afc8603031e9f1 -s b082491da2aefebe79e36e2067d59012 --unique&
+echo ""
+while [[ -z $(grep "done" workspace/42e6efd660ad561189afc8603031e9f1/workspace/b082491da2aefebe79e36e2067d59012/output.txt) ]]; do sleep 10; echo -e "[1A[0K$(grep "freqs" workspace/42e6efd660ad561189afc8603031e9f1/workspace/b082491da2aefebe79e36e2067d59012/output.txt | tail -n 1)"; done
+python fix_subjob_doc.py b082491da2aefebe79e36e2067d59012 42e6efd660ad561189afc8603031e9f1
+git add workspace/42e6efd660ad561189afc8603031e9f1/workspace/b082491da2aefebe79e36e2067d59012/signac_job_document.json
+git add workspace/42e6efd660ad561189afc8603031e9f1/workspace/b082491da2aefebe79e36e2067d59012/output.txt
 echo e2de5f611345565f42e3f3ef592bd814
 git add workspace/42e6efd660ad561189afc8603031e9f1/workspace/e2de5f611345565f42e3f3ef592bd814/signac_statepoint.json
 python recalculate_band_structure.py -j 42e6efd660ad561189afc8603031e9f1 -s e2de5f611345565f42e3f3ef592bd814 --unique&
