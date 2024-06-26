464452bd98f88a311acf7aaeb89b8a911c1b5696
Rose K. Cersonsky <47536110+rosecers@users.noreply.github.com>
2024-06-26 13:28:03 -0500

    Adding jobs for 57656712e08005cc1a135a8455de71c8

--- workspace/57656712e08005cc1a135a8455de71c8/fix.sh
+++
@@ -1,10 +0,0 @@
-echo '57656712e08005cc1a135a8455de71c8: 1 jobs total'
-echo 3dda5a4f6eae439a95e95271bb382630
-git add workspace/57656712e08005cc1a135a8455de71c8/workspace/3dda5a4f6eae439a95e95271bb382630/signac_statepoint.json
-python recalculate_band_structure.py -j 57656712e08005cc1a135a8455de71c8 -s 3dda5a4f6eae439a95e95271bb382630 --unique&
-echo ""
-while [[ -z $(grep "done" workspace/57656712e08005cc1a135a8455de71c8/workspace/3dda5a4f6eae439a95e95271bb382630/output.txt) ]]; do sleep 10; echo -e "[1A[0K$(grep "freqs" workspace/57656712e08005cc1a135a8455de71c8/workspace/3dda5a4f6eae439a95e95271bb382630/output.txt | tail -n 1)"; done
-python fix_subjob_doc.py 3dda5a4f6eae439a95e95271bb382630 57656712e08005cc1a135a8455de71c8
-git add workspace/57656712e08005cc1a135a8455de71c8/workspace/3dda5a4f6eae439a95e95271bb382630/signac_job_document.json
-git add workspace/57656712e08005cc1a135a8455de71c8/workspace/3dda5a4f6eae439a95e95271bb382630/output.txt
-rm /Users/rca/periodic_structures/workspace/57656712e08005cc1a135a8455de71c8/fix.sh
